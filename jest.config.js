c8c0ce9a17d49c182a25710bad9d649971bed655
GHLandy <ghlandy@ghlandy.com>
2019-08-23 23:34:19 +0800

    add jest for test

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  // Prevent tests from printing messages through the console.
+  silent: true,
+
+  // Indicates whether each individual test should be reported during the run
+  verbose: true,
+}
